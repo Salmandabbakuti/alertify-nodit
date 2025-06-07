eb6dbc832414ba67c7cee7b7c53ce8beccdb74ba
Salman Dabbakuti <29351207+Salmandabbakuti@users.noreply.github.com>
2025-06-07 09:41:44 +0530

    added: nextjs boilerplate with prisma

---
+++ client/lib/prisma.js
@@ -0,0 +1,16 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../prisma/generated";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL
+});
+
+const newPrismaClient = new PrismaClient({
+  adapter,
+  log: ["warn", "error"]
+});
+
+const prisma = global.prisma || newPrismaClient;
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
+
+export default prisma;
