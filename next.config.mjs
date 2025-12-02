59d7b0aafa061a29f5ac87610a6136921529d44b
Salman Dabbakuti <29351207+Salmandabbakuti@users.noreply.github.com>
2025-12-02 20:13:54 +0530

    feat: add reactCompiler and serverExternalPackages to nextConfig; update package.json and package-lock.json with babel-plugin-react-compiler

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactCompiler: true,
+  serverExternalPackages: ["pino-pretty", "lokijs", "encoding"]
+};
 
 export default nextConfig;
