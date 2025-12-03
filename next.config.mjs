f5e6d9405cabf24be2d30c985d08c753fb1b10f9
Salman Dabbakuti <29351207+Salmandabbakuti@users.noreply.github.com>
2025-12-03 22:07:03 +0530

    feat: move serverExternalPackages to webpack configuration in nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
-  serverExternalPackages: ["pino-pretty", "lokijs", "encoding"]
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  }
 };
 
 export default nextConfig;
